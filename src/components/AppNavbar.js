9593dcbc453ea831d87cda774877a7156492d573
daniel tingcoy <[danieltingcoy@gmail.com]>
2021-10-10 18:15:49 +0800

    Initial commit

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Bootstrap components
+import Navbar from 'react-bootstrap/Navbar';
+
+
+export default function AppNavbar(){
+	return (
+	<Navbar bg="light" expand="lg">
+		<div>
+			<h1>Real Time Search</h1>
+		</div>
+	</Navbar>
+	)
+};
